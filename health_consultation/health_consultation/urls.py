ef11427c0429ef41e90db6206f9dc56e28371838
Jason Saldua <jason.saldua@chmsu.edu.ph>
2026-05-28 00:12:11 +0800

    Use custom admin routes and remove Django admin links

--- health_consultation/health_consultation/urls.py
+++ health_consultation/health_consultation/urls.py
@@ -1,14 +1,14 @@
 """
 URL configuration for health_consultation project.
 """
-from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', RedirectView.as_view(pattern_name='accounts:admin_login', permanent=False)),
+    path('admin/<path:extra>/', RedirectView.as_view(pattern_name='accounts:admin_login', permanent=False)),
     path('', TemplateView.as_view(template_name='landing.html'), name='home'),
     path('terms/', TemplateView.as_view(template_name='terms.html'), name='terms'),
     path('privacy/', TemplateView.as_view(template_name='privacy.html'), name='privacy'),
